b59380e79d3fa36a2ab53cb1dab45bf01da6fb1d
jorge.pitang <jorge.ori@pitang.com>
2022-08-04 21:55:08 -0300

    criação do interceptor de logs de erro

--- src/app/pages/user/shared/service/user.service.ts
+++ src/app/pages/user/shared/service/user.service.ts
@@ -1,20 +1,22 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AbstractControl, AsyncValidatorFn, ValidationErrors } from '@angular/forms';
-import { of, take, map } from 'rxjs';
+import { of, take, map, BehaviorSubject } from 'rxjs';
 import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  public logs = new BehaviorSubject<any>(null);
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   public existsUserName(): AsyncValidatorFn {
     return (control: AbstractControl): Observable<ValidationErrors | null> => {
       if (!control?.value && control.value === '') {
         return of(null);
-      } 
+      }
       const random = Math.random() < 0.5;
       return of(random).pipe(
         take(1),
@@ -24,4 +26,20 @@ export class UserService {
       );
     };
   }
+
+  public getData(): Observable<any> {
+    const error = new HttpErrorResponse({ status: 422 });
+    //return throwError(() => error);
+
+    const json = {
+      teste: 'teste',
+      parametro: '2344'
+    }
+    return this.http.post<any>('http://teste', json, {observe: 'response'});
+  }
+
+  public getDataUrl(): Observable<any> {
+    return this.http.get<any>('https://jsonplaceholder.typicode.com/todos/1', {observe: 'response'});
+  }
+
 }
